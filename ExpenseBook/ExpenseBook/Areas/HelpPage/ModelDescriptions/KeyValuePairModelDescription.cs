b0931b7b22e101d9f485ad631bd4b6a7fccc7287
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-03-05 13:07:56 +0200

    First
    With Get in API

---
+++ ExpenseBook/ExpenseBook/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ExpenseBook.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
